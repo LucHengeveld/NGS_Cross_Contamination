c157d13838afd47ed9cc957f45797d52c725a8f8
LucHengeveld <luchengeveld@hotmail.com>
2022-09-30 11:24:29 +0200

    Created barcode + spike-in function to compare fastq data to entered barcode and spike-in data. Added comments and docstrings.

--- scripts/check_barcodes.py
+++ scripts/check_barcodes.py
@@ -1,15 +1,20 @@
 from Levenshtein import distance
 import re
+from scripts import spike_in_contamination as sic
 
 
-def barc_no_spike(barcode_file_data, fastq_data, diff_bar_nucl,
-                  correct_i5_list, correct_i7_list, i5_i7_combinations):
-    # TODO: Add last 3 params to docstring
+def barc_no_spike(fastq_data, diff_bar_nucl, correct_i5_list,
+                  correct_i7_list, i5_i7_combinations):
     """
     Checks and counts the different possible i5 + i7 combinations.
-    :param barcode_file_data: List with all barcodes from the Excel file.
     :param fastq_data: List with the structure [barcode 1, barcode2, etc].
     :param diff_bar_nucl: Parameter from parameters.txt.
+    :param correct_i5_list: List with all i5 barcodes from entered barcode
+            file.
+    :param correct_i7_list: List with all i7 barcodes from entered barcode
+            file.
+    :param i5_i7_combinations: Dictionary containing all possible i5 + i7
+            combinations with as value a 0. Structure: {i5: {i7: 0, i7: 0}}.
     :return i5_i7_combinations: Dictionary containing all possible i5 + i7
             combinations and its amount of occurrences in the fastq file.
             Structure: {i5: {i7: counter}, {i7, counter}}.
@@ -22,6 +27,7 @@ def barc_no_spike(barcode_file_data, fastq_data, diff_bar_nucl,
     """
     # TODO: Split function into smaller ones
     # TODO: Filter out unknown barcodes that contain homopolymers (AAAA etc)
+
     # Creates an empty list
     unknown_barcodes = {}
     unknown_i5 = {}
@@ -42,21 +48,21 @@ def barc_no_spike(barcode_file_data, fastq_data, diff_bar_nucl,
 
                 # If i5 is unknown, adds it to a list
                 elif i5 not in correct_i5_list and i7 in correct_i7_list:
-                    if i5 in unknown_i5.keys():
+                    if unknown_i5.get(i5):
                         unknown_i5[i5] += 1
                     else:
                         unknown_i5[i5] = 1
 
                 # If i7 is unknown, adds it to a list
                 elif i5 in correct_i5_list and i7 not in correct_i7_list:
-                    if i7 in unknown_i7.keys():
+                    if unknown_i7.get(i7):
                         unknown_i7[i7] += 1
                     else:
                         unknown_i7[i7] = 1
 
                 # If i5 + i7 both are unknown, it adds the barcode to a list
                 else:
-                    if barcode in unknown_barcodes.keys():
+                    if barcode in unknown_barcodes:
                         unknown_barcodes[barcode] += 1
                     else:
                         unknown_barcodes[barcode] = 1
@@ -88,24 +94,25 @@ def barc_no_spike(barcode_file_data, fastq_data, diff_bar_nucl,
 
                 # If only i7 matches, adds the i5 barcode to a list
                 elif not bool_i5 and bool_i7:
-                    if i5 in unknown_i5.keys():
+                    if i5 in unknown_i5:
                         unknown_i5[i5] += 1
                     else:
                         unknown_i5[i5] = 1
 
                 # If only i5 matches, adds the i7 barcode to a list
                 elif bool_i5 and not bool_i7:
-                    if i7 in unknown_i7.keys():
+                    if i7 in unknown_i7:
                         unknown_i7[i7] += 1
                     else:
                         unknown_i7[i7] = 1
 
                 # If both i5 and i7 do not match, add both to a list
                 else:
-                    if barcode in unknown_barcodes.keys():
+                    if barcode in unknown_barcodes:
                         unknown_barcodes[barcode] += 1
                     else:
                         unknown_barcodes[barcode] = 1
+
     except ValueError:
         print("Error 12: Barcodes have not been found at the end of the "
               "headers in the fastq file.")
@@ -115,32 +122,161 @@ def barc_no_spike(barcode_file_data, fastq_data, diff_bar_nucl,
     return i5_i7_combinations, unknown_barcodes, unknown_i5, unknown_i7
 
 
-def barc_with_spike(barcode_file_data, fastq_data, diff_bar_nucl,
-                    diff_spike_nucl):
-    # TODO: Docstrings and comments
-    # output: list with dictionaries [i5+i7+spike, i5+spike, i7+spike, unknown barcodes/spike ins etc]
-    output_dicts = ["i5_i7_spike", "i5_spike", "i7_spike", "unknown_i5",
-                    "unknown_i7", "unknown_spike", "Unknown_i5_i7",
-                    "unknown_i5_spike", "unknown_i7_spike",
-                    "unknown_i5_i7_spike"]
-    output_data = {}
-    for dict_name in output_dicts:
-        output_data[dict_name] = None
-    print(output_data)
+def barc_with_spike(combinations, correct_spike_list, correct_i5_list,
+                    correct_i7_list, fastq_data, diff_bar_nucl, diff_seq_nucl,
+                    spike_ins):
+    """
+    Compares all fastq barcodes and spike-ins sequences to the barcode and
+    spike-ins sequence file.
+    :param combinations: Dictionary containing every possible barcode +
+            spike-in sequence combination. Structure depends on spike-ins
+            parameter.
+    :param correct_spike_list: List with all spike-in sequences from the
+            entered barcode file.
+    :param correct_i5_list: List with all i5 barcodes from the entered barcode
+            file.
+    :param correct_i7_list: List with all i7 barcodes from the entered barcode
+            file.
+    :param fastq_data: List with the structure [[i5, i7, sequence],[i5, i7,
+            sequence], etc].
+    :param diff_bar_nucl: Parameter from parameters.txt.
+    :param diff_seq_nucl: Parameter from parameters.txt.
+    :param spike_ins: Parameter from parameters.txt.
+    :return unknown_dict: Dictionary containing all unknown barcodes and
+            spike-in sequences.
+    :return combinations: Dictionary containing every possible barcode +
+            spike-in sequence combination and the amount of occurrences.
+            Structure depends on spike-ins parameter.
+    """
+    # List of all possible unknown barcode / spike-in sequence
+    # combinations
+    unknown_keys = ["i5", "i7", "spike", "i5_i7", "i5_spike", "i7_spike",
+                    "i5_i7_spike"]
 
-    # eerste 3 opslaan als dicts want barcodes en sequenties bekend,
-    # rest als lijsten want barcodes en sequenties onbekend
-    pass
+    # Loops through the possible unknown combinations and adds these to
+    # a dictionary
+    unknown_dict = {}
+    for key in unknown_keys:
+        unknown_dict[key] = {}
+
+    # Loops through the fastq data list
+    for read in fastq_data:
+
+        # Saves the spike-in sequence as variable
+        spike_seq = read[2]
+
+        # Checks if spike-ins parameter has value 2 (i5+spike) or 3
+        # (i7+spike)
+        if spike_ins in [2, 3]:
+            if spike_ins == 2:
+                # Saves the i5 barcode and correct barcode list to a
+                # variable
+                barcode = read[0]
+                bar_type = "i5"
+                correct_bar_list = correct_i5_list
+
+            else:
+                # Saves the i7 barcode and correct barcode list to a
+                # variable
+                barcode = read[1]
+                bar_type = "i7"
+                correct_bar_list = correct_i7_list
+
+            # Checks if barcode nucleotides are allowed to differ
+            if diff_bar_nucl == 0:
+
+                # Calls correct functions depending on diff_bar_nucl and
+                # diff_seq_nucl parameters
+                if diff_seq_nucl == 0:
+
+                    # Bar diff = 0 and seq diff = 0
+                    unknown_dict, combinations = sic.bar_spike(
+                        barcode, bar_type, combinations, spike_seq,
+                        unknown_dict, correct_bar_list, correct_spike_list)
+                else:
+                    # Bar diff = 0 and seq diff != 0
+                    unknown_dict, combinations = sic.bar_spike_seq_diff(
+                        barcode, bar_type, combinations, spike_seq,
+                        unknown_dict, correct_bar_list, correct_spike_list,
+                        diff_seq_nucl)
+
+            # Calls correct functions depending on diff_bar_nucl and
+            # diff_seq_nucl parameters
+            elif diff_seq_nucl == 0:
+
+                # Bar diff != 0 and seq diff = 0
+                unknown_dict, combinations = sic.bar_spike_bar_diff(
+                    barcode, bar_type, combinations, spike_seq, unknown_dict,
+                    correct_bar_list, correct_spike_list, diff_bar_nucl)
+
+            else:
+                # Bar diff != 0 and seq diff != 0
+                unknown_dict, combinations = sic.bar_spike_bar_seq_diff(
+                    barcode, bar_type, combinations, spike_seq, unknown_dict,
+                    correct_bar_list, correct_spike_list, diff_bar_nucl,
+                    diff_seq_nucl)
+
+        # Calls the i5+i7+spike-ins functions
+        else:
+
+            # Saves the i5+i7 barcode to a variable
+            barcode = read[0] + "+" + read[1]
+
+            # Calls correct functions depending on diff_bar_nucl and
+            # diff_seq_nucl parameters
+            if diff_bar_nucl == 0:
+                if diff_seq_nucl == 0:
+
+                    # Bar diff = 0 and seq diff = 0
+                    unknown_dict, combinations = sic.i5_i7_spike(
+                        barcode, combinations, spike_seq, unknown_dict,
+                        correct_i5_list, correct_spike_list)
+
+                else:
+                    # Bar diff = 0 and seq diff != 0
+                    unknown_dict, combinations = sic.i5_i7_spike_seq_diff(
+                        barcode, combinations, spike_seq, unknown_dict,
+                        correct_i5_list, correct_spike_list, diff_seq_nucl)
+
+            # Calls correct functions depending on diff_bar_nucl and
+            # diff_seq_nucl parameters
+            elif diff_seq_nucl == 0:
+
+                # Bar diff != 0 and seq diff = 0
+                unknown_dict, combinations = sic.i5_i7_spike_bar_seq_diff(
+                    barcode, combinations, spike_seq, unknown_dict,
+                    correct_i5_list, correct_spike_list, diff_bar_nucl,
+                    diff_seq_nucl)
+
+            else:
+                # Bar diff != 0 and seq diff != 0
+                unknown_dict, combinations = sic.i5_i7_spike_bar_diff(
+                    barcode, combinations, spike_seq, unknown_dict,
+                    correct_i5_list, correct_spike_list, diff_bar_nucl)
+
+    # Returns a dictionary containing all unknown i5/i7/spike-in
+    # combinations and a dictionary containing all known combinations
+    # and their occurrence
+    return unknown_dict, combinations
 
 
-def retrieve_correct_barcodes_combinations(barcode_file_data):
-    # TODO: Docstrings
+def retrieve_combinations_no_spike(barcode_file_list):
+    """
+    Creates a dictionary with all possible barcode combinations.
+    :param barcode_file_list: List with all barcodes from the Excel file.
+    :return correct_i5_list: List with all i5 barcodes from entered barcode
+            file.
+    :return correct_i7_list: List with all i7 barcodes from entered barcode
+            file.
+    :return i5_i7_combinations: Dictionary containing all possible i5 + i7
+            combinations with as value a 0. Structure: {i5: {i7: 0, i7: 0}}.
+    """
     # Creates empty lists
     correct_i5_list = []
     correct_i7_list = []
 
     # Splits the barcodes from the Excel file into i5 and i7
-    for line in barcode_file_data:
+    for line in barcode_file_list:
         i5, i7 = line[1].split("+")
         correct_i5_list.append(i5)
         correct_i7_list.append(i7)
@@ -152,12 +288,79 @@ def retrieve_correct_barcodes_combinations(barcode_file_data):
     # set the amount of occurrences to 0
     for i5 in correct_i5_list:
         for i7 in correct_i7_list:
-            if i5 not in i5_i7_combinations.keys():
+            if i5 not in i5_i7_combinations:
                 i5_i7_combinations[i5] = {i7: 0}
             else:
                 i5_i7_combinations[i5][i7] = 0
 
+    # Returns the correct barcode lists and i5_i7_combinations dict
     return correct_i5_list, correct_i7_list, i5_i7_combinations
+
+
+def retrieve_combinations_with_spike(barcode_file_dict, spike_ins):
+    """
+    Creates a dictionary with all possible barcode combinations.
+    :param barcode_file_dict: List with all barcodes from the Excel file.
+    :param spike_ins: Parameter from parameters.txt.
+    :return combinations: Dictionary containing every possible barcode +
+            spike-in sequence combination. Structure depends on spike-ins
+            parameter.
+    :return correct_spike_list: List with all spike-in sequences from the
+            entered barcode file.
+    :return correct_i5_list: List with all i5 barcodes from the entered barcode
+            file.
+    :return correct_i7_list: List with all i7 barcodes from the entered barcode
+            file.
+    """
+    # Creates empty lists
+    correct_i5_list = []
+    correct_i7_list = []
+    correct_spike_list = []
+
+    # Splits the barcodes from the Excel file into i5 and i7
+    for barcode in barcode_file_dict:
+        i5, i7 = barcode.split("+")
+        correct_i5_list.append(i5)
+        correct_i7_list.append(i7)
+        correct_spike_list.append(barcode_file_dict[barcode][1])
+
+    # Creates empty dictionary
+    combinations = {}
+
+    # Creates a dictionary with every possible combination and set the
+    # amount of occurrences to 0
+    if spike_ins in [2, 3]:
+
+        # Use correct list depending on spike_ins parameter
+        if spike_ins == 2:
+            correct_barcode_list = correct_i5_list
+        else:
+            correct_barcode_list = correct_i7_list
+
+        # Saves the i5 + spike-in or i7 + spike-in combinations to a
+        # dictionary
+        for barcode in correct_barcode_list:
+            for spike_seq in correct_spike_list:
+                if barcode not in combinations:
+                    combinations[barcode] = {spike_seq: 0}
+                else:
+                    combinations[barcode][spike_seq] = 0
+
+    else:
+        # Saves the i5 / i7 / spike-in sequence combinations to a
+        # dictionary
+        for bar_index in range(len(correct_i5_list)):
+            for spike_seq in correct_spike_list:
+                barcode = correct_i5_list[bar_index] + "+" + correct_i7_list[
+                    bar_index]
+                if barcode not in combinations:
+                    combinations[barcode] = {spike_seq: 0}
+                else:
+                    combinations[barcode][spike_seq] = 0
+
+    # Returns the combinations dict and correct i5, i7 and spike
+    # sequence lists
+    return combinations, correct_spike_list, correct_i5_list, correct_i7_list
 
 
 def retrieve_barcode_location(barcode_file_data):
@@ -174,8 +377,8 @@ def retrieve_barcode_location(barcode_file_data):
 
     # Loops through the barcodes from the entered barcode Excel file
     for barcode in barcode_file_data:
-        i5_loc = re.split('(\d+)', barcode[0])[0]
-        i7_loc = re.split('(\d+)', barcode[0])[1]
+        i5_loc = re.split("(\d+)", barcode[0])[0]
+        i7_loc = re.split("(\d+)", barcode[0])[1]
         i5_bar, i7_bar = barcode[1].split("+")
 
         # Adds the barcodes and their well locations to the dictionary
