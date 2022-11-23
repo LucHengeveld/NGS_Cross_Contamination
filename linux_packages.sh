7095d14583c8bf7c6ea5ae928c95c84a58df9b56
LucHengeveld <luchengeveld@hotmail.com>
2022-11-23 11:44:18 +0100

    Created batch / shell scripts to install all requirements.

---
+++ linux_packages.sh
@@ -0,0 +1,6 @@
+# This script downloads the required packages for a Linux operating system.
+#! usr/bin/bash
+pip install Levenshtein==0.20.8
+pip install openpyxl==3.0.10
+pip install XlsxWriter==3.0.3
+pip install python-settings==0.2.2
---
+++ windows_packages.bat
@@ -0,0 +1,5 @@
+:: This script downloads the required packages for a Windows operating system.
+pip install Levenshtein==0.20.8
+pip install openpyxl==3.0.10
+pip install XlsxWriter==3.0.3
+pip install python-settings==0.2.2
